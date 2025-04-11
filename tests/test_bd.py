aaf0457a5ce3afd32ed3eaeed4279b05bea447d0
daria <daria_dubasova@vortex.foundation>
2025-04-11 15:47:18 +0300

    Добавлен первый рабочий тест для бд

--- tests/test_bd.py
+++ tests/test_bd.py
@@ -1,11 +1,8 @@
 import sqlite3
-import pytest
 from db.db_utils import add_user, add_order
-from conftest import setup_db
 
-class dbTestind:
+class TestDB:
     def test_user_in_db(self, setup_db):
-        with allure.step("Добавляем пользователя в бд"):
             add_user("Аня", "anya@example.com", 17)
             conn = sqlite3.connect("test.db")
             cursor = conn.cursor()
@@ -18,7 +15,15 @@ class dbTestind:
             assert user[2] == "anya@example.com", "Email не соответствует ожидаемому"
             assert user[3] == 17, "Возраст не соответствует ожидаемому"
 
-        conn.close()
+    def test_user_in_db(self, setup_db):
+            add_user("Аня", "anya@example.com", 17)
+            conn = sqlite3.connect("test.db")
+            cursor = conn.cursor()
 
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
+            cursor.execute("SELECT * FROM users WHERE name = ?", ("Аня",))
+            user = cursor.fetchone()
+
+            assert user is not None, "Такого пользователя нет"
+            assert user[1] == "Аня", "Имя не соответствует ожидаемому"
+            assert user[2] == "anya@example.com", "Email не соответствует ожидаемому"
+            assert user[3] == 17, "Возраст не соответствует ожидаемому"
